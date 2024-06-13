7a8d66b185d774d58292ab6243edf1cd5a8ad246
Steven Ramdas <steven.ramdas@daistra.com>
2024-06-13 21:44:26 +0200

    Remove setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-# setup.py
-from setuptools import setup, find_packages
+# # setup.py
+# from setuptools import setup, find_packages
 
-setup(
-    name="omni-ruff",
-    version="0.1",
-    packages=find_packages(include=["hooks.*"]),
-    author="Steven Ramdas",
-    entry_points={
-        "console_scripts": [
-            "copy-ruff-config=hooks.copy_ruff_config:main",
-        ],
-    },
-)
+# setup(
+#     name="omni-ruff",
+#     version="0.1",
+#     packages=find_packages(include=["hooks.*"]),
+#     author="Steven Ramdas",
+#     entry_points={
+#         "console_scripts": [
+#             "copy-ruff-config=hooks.copy_ruff_config:main",
+#         ],
+#     },
+# )
