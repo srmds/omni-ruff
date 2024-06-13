d6130b86ca335ba7982982941459d6f5428e4111
Steven Ramdas <steven.ramdas@daistra.com>
2024-06-13 18:06:13 +0200

    Fix hook def

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='omni-ruff-py',
+    package_dir={'': '.'},
     version='0.1',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'omni-ruff-py=hooks.copy_ruff_config:main',
+            'omni-ruff-py=hooks.copy_ruff_config',
         ],
     },
 )
