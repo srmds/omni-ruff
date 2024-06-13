1b0b175959a2deb796a0fba4886437d6ad67c777
Steven Ramdas <steven.ramdas@daistra.com>
2024-06-13 18:25:10 +0200

    Fix path

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'hooks/copy_ruff_config.py',
+            'hooks.copy_ruff_config',
         ],
     },
 )
