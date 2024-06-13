72ca9c898629c97c91e87190458eeeb3685fbf8a
Steven Ramdas <steven.ramdas@daistra.com>
2024-06-13 20:26:50 +0200

    Fix path

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 # setup.py
 from setuptools import setup, find_packages
-from os import path
 
 setup(
     name="omni-ruff",
     version="0.1",
-    packages=find_packages(include=["hooks.*"]),
+    packages=find_packages(),
     author="Steven Ramdas",
     python_requires=">=3.9",
     entry_points={
         "console_scripts": [
-            "copy-ruff-config=hooks.copy_ruff_config:main",
+            "copy-ruff-config=copy_ruff_config:main",
         ],
     },
 )
