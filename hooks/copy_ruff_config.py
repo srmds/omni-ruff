55cad4ac7c6294e22fc1e46335737824364d801b
Steven Ramdas <steven.ramdas@daistra.com>
2024-06-13 18:04:02 +0200

    Fix hook def

--- hooks/copy_ruff_config.py
+++ hooks/copy_ruff_config.py
@@ -4,7 +4,6 @@ import subprocess
 from pathlib import Path
 
 def main():
-
     repo_root = Path(subprocess.run("REPO_ROOT=$(git rev-parse --show-toplevel) && echo $REPO_ROOT",shell=True, capture_output=True).stdout.decode('utf-8').rstrip())
     hook_root = Path(Path.cwd())
     source_conf = hook_root / "hooks" / "ruff.toml"
@@ -16,7 +15,12 @@ def main():
     # copy the contents of the demo.py file to  a new file called demo1.py
     shutil.copyfile(str(source_conf), str(dest_conf))
 
-    sys.exit(0)
 
 if __name__ == '__main__':
-    main()
+    try:
+         main()
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+        
+    sys.exit(0)
