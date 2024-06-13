1f5f1ab2283cab0e0037c98fbfcfe4af670deca7
Steven Ramdas <steven.ramdas@daistra.com>
2024-06-13 18:00:10 +0200

    Fix hook def

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'omni-ruff-py=hooks.copy_ruff_config.main',
+            'omni-ruff-py=hooks.copy_ruff_config:main',
         ],
     },
 )
