916ff2dcb715eb29e5ccb623f258b0048e321c0e
Steven Ramdas <steven.ramdas@daistra.com>
2024-06-13 21:53:29 +0200

    Tweak entry

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 setup(
     name="omni-ruff",
     version="0.1",
-    packages=find_packages(include=["hooks.*"]),
+    packages=find_packages(),
     author="Steven Ramdas",
     entry_points={
         "console_scripts": [
-            "copy-ruff-config=hooks.copy_ruff_config:main",
+            "copy-ruff-config=copy_ruff_config:main",
         ],
     },
 )
