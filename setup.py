892d2d1ce3f930f74738a7c494ab471fbb7d10c6
Steven Ramdas <steven.ramdas@daistra.com>
2024-06-14 00:01:29 +0200

    Bump release version to 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="omni-ruff",
-    version="0.1",
+    version="0.2.0",
     packages=find_packages(),
     author="Steven Ramdas",
     entry_points={
