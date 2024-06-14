202a8c2bcd5553a6e03a0c2d02669c2ffb8f40ac
Steven Ramdas <steven.ramdas@daistra.com>
2024-06-14 03:59:53 +0200

    Migrate to hooks folder

--- hooks/clone_ruff_config.py
+++ hooks/clone_ruff_config.py
@@ -69,6 +69,9 @@ def main(argv=None):
             import platform
 
             temp_dir = Path("/tmp" if platform.system() == "Darwin" else tempfile.gettempdir())
+            if Path(temp_dir).exists():
+                Path(temp_dir).rmdir()
+                
             Path(temp_dir).mkdir(exist_ok=True)
             
             try:
